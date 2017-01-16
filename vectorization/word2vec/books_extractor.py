3512e078442924eeee3ecb0ecd8d9c1822cb5333
neocode <sheremet-a@mail.ru>
2017-01-16 23:07:19 +0200

    Some comments added.

--- vectorization/word2vec/books_extractor.py
+++ vectorization/word2vec/books_extractor.py
@@ -1,3 +1,5 @@
+# Extract sentences from books.
+
 import sys
 import codecs
 import string
@@ -7,21 +9,30 @@ from spacy.en import English
 
 
 def extract(data_path):
+    """
+    Extract sentences from books.
+    :param data_path: path to the dir with books.
+    """
+    # The first allowable character in a sentence
     header_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
                    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
                    'U', 'V', 'W', 'X', 'Y', 'Z', '"', "'"]
     nlp = English()
+    # List names of the files
     list_dir = os.listdir(data_path)
     print('There are %s files for training.' % len(list_dir))
     sent_num = 0
+    # Create the output file with sentences.
     output_file = codecs.open('sentences.txt', encoding='utf-8', mode='w')
     for file in list_dir:
+        # Make path for every file.
         file_path = '/'.join([data_path, file])
         input_file = codecs.open(file_path, encoding='utf-8', mode='r', errors='ignore')
         text = []
         for line in input_file:
             text.append(line.strip())
         text = ' '.join(text)
+        # Make Spacy doc-object
         doc = nlp(text)
         sentences = []
         for sent in doc.sents:
@@ -30,6 +41,8 @@ def extract(data_path):
             sent = sent.strip()
             sent = sent.replace("\t", " ")
             sent = re.sub(' +', ' ', sent)
+            # Take only sentences with length more then 10 words with first symbol
+            # in the header_list and second symbol - not in the header list.
             if (len(sent) > 10) and (sent[0] in header_list) and (sent[1] not in header_list):
                 output_file.write(sent + '\n')
                 sent_num += 1
--- vectorization/word2vec/w2v_maker.py
+++ vectorization/word2vec/w2v_maker.py
@@ -1,22 +1,43 @@
+# Make Word2Vec model.
+
 import os
 import codecs
+import shutil
 import nltk.tokenize
 
 from gensim.models import Word2Vec
 
 
+# A tokenizer that splits a string using a regular expression, which
+# matches either the tokens or the separators between tokens.
+
 _tokenizer = nltk.tokenize.RegexpTokenizer(pattern=r'[\w\$]+|[^\w\s]')
+
+# Parameters of Word2Vec model:
+# - save_path - the path for model saving;
+# - vect_size - the dimensionality of the feature vectors
+# - min_count - ignore all words with total frequency lower than this;
+# - max_vocab_size - limit RAM during vocabulary building; if there are more unique words than this,
+#   then prune the infrequent ones. Every 10 million word types need about 1GB of RAM.
+#   Set to None for no limit (default).
+# - window - the maximum distance between the current and predicted word within a sentence
+# - workers_num - use this many worker threads to train the model
 params = {
     "save_path": './w2v/w2v_model.bin',
     "vect_size": 256,
     "min_count": 1,
-    "vocab_max_size": 10000,
-    "win_size": 5,
-    "workers_num": 25
+    "max_vocab_size": 100000,
+    "window": 5,
+    "workers_num": 4
 }
 
 
 def prepare_data(file_path):
+    """
+    Prepare data for making Word2vec model
+    :param file_path: path to the file with data for making model
+    :return: tokenized sentences
+    """
     input_file = codecs.open(file_path, encoding='utf-8', mode='r', errors='ignore')
     result = []
     for line in input_file:
@@ -26,10 +47,15 @@ def prepare_data(file_path):
 
 
 def w2v_train(tokenized_lines):
+    """
+    Make the Word2Vec model on the base of tokenized sentences
+    :param tokenized_lines: tokenized sentences
+    :return: Word2Vec model
+    """
     dir_remove('./w2v')
-    model = Word2Vec(window=int(params['win_size']),
+    model = Word2Vec(window=int(params['window']),
                      min_count=int(params['min_count']),
-                     max_vocab_size=int(params['vocab_max_size']),
+                     max_vocab_size=int(params['max_vocab_size']),
                      size=int(params['vect_size']),
                      workers=int(params['workers_num']))
     model.build_vocab(tokenized_lines)
@@ -39,6 +65,11 @@ def w2v_train(tokenized_lines):
 
 
 def dir_remove(path):
+    """
+    Remove the dir with model with all subdirs if it exists.
+    Make the dir for model if it is not exists.
+    :param path: path to the model
+    """
     if (os.path.exists(path)) and (os.listdir(path) != []):
         shutil.rmtree(path)
         os.makedirs(path)
